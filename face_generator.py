7a1b48da46ba25cc74dc212811ed3c5524339c26
lukoq <kinderlukasz01@gmail.com>
2024-10-05 17:46:31 +0200

    update

--- face_generator.py
+++ face_generator.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import torchvision
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
@@ -19,25 +20,23 @@ class Generator(nn.Module):
         self.network = nn.Sequential(
             # Warstwa w pełni połączona: przekształcenie wejścia do 7x7x256
             nn.Linear(input_dim, 7 * 7 * 256),
-            nn.ReLU(),
-
             # Przekształcenie tensora do wymiarów (batch_size, 256, 7, 7)
             nn.Unflatten(dim=1, unflattened_size=(256, 7, 7)),
 
             # Warstwa transponowanej konwolucji: (256, 7, 7) -> (128, 14, 14)
             nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
 
             # Warstwa transponowanej konwolucji: (128, 14, 14) -> (64, 28, 28)
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
 
             # Piąta warstwa konwolucji: Dodatkowa konwolucja dla większej złożoności (64, 28, 28) -> (32, 28, 28)
             nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
+            nn.BatchNorm2d(32, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
 
 
             # Ostatnia warstwa transponowanej konwolucji: (32, 28, 28) -> (1, 28, 28)
@@ -75,27 +74,6 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.network(x)
-
-
-def evolve_generators(generators, fitness_scores, mutation_rate=0.05):
-    # Selekcja elitarnych generatorów
-    elite_idx = np.argsort(fitness_scores)[-2:]  # Najlepsze 2 generatory
-    elite_generators = [generators[i] for i in elite_idx]
-
-    # Mutacja pozostałych generatorów
-    new_generators = []
-    for _ in range(len(generators) - len(elite_generators)):
-        parent = np.random.choice(elite_generators)
-        new_gen = Generator(input_dim=100)
-        new_gen.load_state_dict(parent.state_dict())
-
-        # Wprowadź mutacje do wag sieci
-        for param in new_gen.parameters():
-            param.data += mutation_rate * torch.randn_like(param)
-
-        new_generators.append(new_gen)
-
-    return elite_generators + new_generators
 
 
 def train_discriminator(discriminator, real_data, fake_data, optimizer, loss_fn):
@@ -179,60 +157,44 @@ transform = transforms.Compose([
 dataset = datasets.ImageFolder(root='data/faces', transform=transform)
 data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
 
-num_generators = 1
-generators = [Generator(input_dim=100).to(device) for _ in range(num_generators)]
+generator = Generator(100).to(device)
 
 # Inicjalizacja dyskryminatora
 discriminator = Discriminator().to(device)
 
 d_optimizer = optim.Adam(discriminator.parameters(), lr=1e-4, betas=(0.9, 0.999))
-g_optimizers = [optim.Adam(gen.parameters(), lr=1e-4, betas=(0.9, 0.999)) for gen in generators]
+g_optimizers = optim.Adam(generator.parameters(), lr=1e-4, betas=(0.9, 0.999))
 
 loss_fn = nn.BCELoss()
 g_losses = []
 d_losses = []
 
-for epoch in range(1000):  # Liczba epok treningowych
+for epoch in range(150):  # Liczba epok treningowych
     g_epoch_loss = 0.0
     d_epoch_loss = 0.0
     for real_images, _ in data_loader:
         real_images = real_images.to(device)
         # Trenuj dyskryminator
-        if epoch % 10 == 0:
-            # print("Ucze dyskryminator...")
-            for gen in generators:
-                gen.apply(weights_init)
-                noise = torch.randn(64, 100).to(device)
-                fake_images = gen(noise).detach()
-                d_loss = train_discriminator(discriminator, real_images, fake_images, d_optimizer, loss_fn)
-                d_epoch_loss += d_loss  # Zaktualizuj stratę dyskryminatora
+        if epoch % 3 == 0:
+            noise = torch.randn((64, 100)).to(device)
+            fake_images = generator(noise).detach()
+            d_loss = train_discriminator(discriminator, real_images, fake_images, d_optimizer, loss_fn)
+            d_epoch_loss += d_loss  # Zaktualizuj stratę dyskryminatora
 
-
-        # Trenuj generatory i ewaluacja
+        # Trenuj generator
         fitness_scores = []
-        for i, gen in enumerate(generators):
-            g_loss = train_generator(gen, discriminator, g_optimizers[i], loss_fn)
-            g_epoch_loss += g_loss  # Zaktualizuj stratę generatora
-            fitness_score = evaluate_generator(gen, discriminator)
-            fitness_scores.append(fitness_score)
+        g_loss = train_generator(generator, discriminator, g_optimizers, loss_fn)
+        g_epoch_loss += g_loss  # Zaktualizuj stratę generatora
+        fitness_score = evaluate_generator(generator, discriminator)
+        fitness_scores.append(fitness_score)
 
-        print(fitness_scores)
-
-        # Ewolucja generatorów
-        generators = evolve_generators(generators, fitness_scores)
+        # print(fitness_scores)
 
     # Zapisz średnie straty dla generatora i dyskryminatora w tej epoce
     g_losses.append(g_epoch_loss / len(data_loader))
     d_losses.append(d_epoch_loss / len(data_loader))
 
-    # Rysuj  co 100 epok
-
-    if epoch % 100 == 0:
-        show_images(generators[0])
-
-
     print(f'Epoch {epoch + 1} has ended.')
 
 # Wyświetlanie najlepszego generatora
-best_gen = generators[np.argmax(fitness_scores)]
-show_images(best_gen)
+show_images(generator)
