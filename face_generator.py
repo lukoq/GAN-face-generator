71b88746c6505a88066293983777cf6a98281a6b
lukoq <kinderlukasz01@gmail.com>
2024-10-06 14:16:24 +0200

    U-Net Gan added

--- face_generator.py
+++ face_generator.py
@@ -1,96 +1,24 @@
-import torch
-import torch.nn as nn
 import torch.optim as optim
-import numpy as np
 import matplotlib.pyplot as plt
 import torchvision
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import torch.nn.functional as F
+from models import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 
-class Generator(nn.Module):
-    def __init__(self, input_dim):
-        super(Generator, self).__init__()
-        self.input_dim = input_dim
-
-        self.network = nn.Sequential(
-            # Warstwa w pełni połączona: przekształcenie wejścia do 7x7x256
-            nn.Linear(input_dim, 7 * 7 * 256),
-            # Przekształcenie tensora do wymiarów (batch_size, 256, 7, 7)
-            nn.Unflatten(dim=1, unflattened_size=(256, 7, 7)),
-
-            # Warstwa transponowanej konwolucji: (256, 7, 7) -> (128, 14, 14)
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # Warstwa transponowanej konwolucji: (128, 14, 14) -> (64, 28, 28)
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # Piąta warstwa konwolucji: Dodatkowa konwolucja dla większej złożoności (64, 28, 28) -> (32, 28, 28)
-            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-
-
-            # Ostatnia warstwa transponowanej konwolucji: (32, 28, 28) -> (1, 28, 28)
-            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=1, padding=1),
-            nn.Tanh()  # Normalizacja do zakresu [-1, 1]
-        )
-
-    def forward(self, x):
-        x = self.network(x)
-        return x
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),  # 1 kanał (szarość) -> 64 kanały
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # 64 -> 128
-            nn.LeakyReLU(0.2),
-
-            # Dodana nowa warstwa konwolucyjna
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # 128 -> 256 kanałów
-            nn.LeakyReLU(0.2),
-
-            # Czwarta warstwa konwolucyjna (opcjonalna dla zwiększenia głębokości)
-            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),  # 4x4 -> 2x2
-            nn.LeakyReLU(0.2),
-
-            nn.Flatten(),  # Spłaszczenie wyjścia
-            nn.Linear(512 * 2 * 2, 1),  # 7x7 po konwolucjach
-            nn.Sigmoid()  # Wartość prawdopodobieństwa
-        )
-
-    def forward(self, x):
-        return self.network(x)
-
-
 def train_discriminator(discriminator, real_data, fake_data, optimizer, loss_fn):
     optimizer.zero_grad()
 
-    # Przekonwertuj rzeczywiste dane do formatu 4D
-    real_data = real_data.view(-1, 1, 28, 28)  # (batch_size, channels, height, width)
-
-    # Prawdziwe dane
+    real_data = real_data.view(-1, 1, 32, 32)
     real_preds = discriminator(real_data)
-    real_loss = loss_fn(real_preds, torch.ones_like(real_preds) * 0.9)  # Label smoothing dla prawdziwych danych
+    real_loss = loss_fn(real_preds, torch.ones_like(real_preds) * 0.9)
 
-    # Fałszywe dane
     fake_preds = discriminator(fake_data)
-    fake_loss = loss_fn(fake_preds, torch.zeros_like(fake_preds) + 0.1)  # Label smoothing dla fałszywych danych
+    fake_loss = loss_fn(fake_preds, torch.zeros_like(fake_preds) + 0.1)
 
-    # Zsumowanie strat i optymalizacja
     total_loss = real_loss + fake_loss
     total_loss.backward()
     optimizer.step()
@@ -101,39 +29,28 @@ def train_discriminator(discriminator, real_data, fake_data, optimizer, loss_fn)
 def train_generator(generator, discriminator, optimizer, loss_fn):
     optimizer.zero_grad()
 
-    # Wygenerowanie fałszywych danych
-    noise = torch.randn(64, 100).to(device)  # Losowy wektor o wymiarach 100
-    fake_data = generator(noise)  # Fałszywe dane o wymiarach (64, 1, 28, 28)
+    noise = torch.randn(64, 1, 32, 32).to(device)
+    fake_data = generator(noise)
 
-    # Dyskryminator ocenia fałszywe dane
     fake_preds = discriminator(fake_data)
 
-    # Chcemy, by dyskryminator uznał fałszywe dane za prawdziwe
     loss = loss_fn(fake_preds, torch.ones_like(fake_preds))
 
-    # Optymalizacja generatora
     loss.backward()
     optimizer.step()
 
     return loss.item()
 
 
-# Funkcja ewaluacji generatora (fitness)
 def evaluate_generator(generator, discriminator):
-    noise = torch.randn(64, 100).to(device)
+    noise = torch.randn(64, 1, 32, 32).to(device)
     fake_data = generator(noise)
     fake_preds = discriminator(fake_data)
-
     return fake_preds.mean().item()
 
 
-def weights_init(m):
-    if isinstance(m, nn.Linear):
-        nn.init.xavier_normal_(m.weight)
-
-
 def show_images(generator):
-    noise = torch.randn(16, 100).to(device)
+    noise = torch.randn(16, 1, 32, 32).to(device)
     fake_images = generator(noise)
 
     # Tworzenie siatki obrazów
@@ -149,34 +66,34 @@ def show_images(generator):
 
 transform = transforms.Compose([
     transforms.Grayscale(),  # Konwersja do skali szarości
-    transforms.Resize((28, 28)),  # Zmiana rozmiaru obrazów do 28x28
+    transforms.Resize((32, 32)),  # Zmiana rozmiaru obrazów do 32x32
     transforms.ToTensor(),  # Konwersja do tensora
     transforms.Normalize((0.5,), (0.5,))  # Normalizacja do zakresu [-1, 1]
 ])
 
-dataset = datasets.ImageFolder(root='data/faces', transform=transform)
+dataset = datasets.ImageFolder(root='data/yale', transform=transform)
 data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
 
-generator = Generator(100).to(device)
+generator = Generator().to(device)
 
 # Inicjalizacja dyskryminatora
 discriminator = Discriminator().to(device)
 
-d_optimizer = optim.Adam(discriminator.parameters(), lr=1e-4, betas=(0.9, 0.999))
-g_optimizers = optim.Adam(generator.parameters(), lr=1e-4, betas=(0.9, 0.999))
+d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0001, betas=(0.5, 0.999))
+g_optimizers = optim.Adam(generator.parameters(), lr=0.0001, betas=(0.5, 0.999))
 
 loss_fn = nn.BCELoss()
 g_losses = []
 d_losses = []
 
-for epoch in range(150):  # Liczba epok treningowych
+for epoch in range(1000):  # Liczba epok treningowych
     g_epoch_loss = 0.0
     d_epoch_loss = 0.0
     for real_images, _ in data_loader:
         real_images = real_images.to(device)
         # Trenuj dyskryminator
-        if epoch % 3 == 0:
-            noise = torch.randn((64, 100)).to(device)
+        if epoch % 2 == 0:
+            noise = torch.randn(64, 1, 32, 32).to(device)
             fake_images = generator(noise).detach()
             d_loss = train_discriminator(discriminator, real_images, fake_images, d_optimizer, loss_fn)
             d_epoch_loss += d_loss  # Zaktualizuj stratę dyskryminatora
@@ -188,13 +105,12 @@ for epoch in range(150):  # Liczba epok treningowych
         fitness_score = evaluate_generator(generator, discriminator)
         fitness_scores.append(fitness_score)
 
-        # print(fitness_scores)
+        #print(fitness_scores)
 
     # Zapisz średnie straty dla generatora i dyskryminatora w tej epoce
     g_losses.append(g_epoch_loss / len(data_loader))
     d_losses.append(d_epoch_loss / len(data_loader))
 
     print(f'Epoch {epoch + 1} has ended.')
-
-# Wyświetlanie najlepszego generatora
-show_images(generator)
+    if epoch > 300 and epoch % 20 == 0:
+        show_images(generator)
---
+++ models.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        # Warstwy enkodera
+        self.encoder1 = self.encoder_block(1, 64)  # 32x32 -> 16x16
+        self.encoder2 = self.encoder_block(64, 128)  # 16x16 -> 8x8
+        self.encoder3 = self.encoder_block(128, 256)  # 8x8 -> 4x4
+
+        # Warstwa bottleneck
+        self.bottleneck = self.bottleneck_block(256, 512)  # 4x4 -> 2x2
+
+        # Warstwy dekodera
+        self.decoder3 = self.decoder_block(512, 256)  # 2x2 -> 4x4
+        self.decoder2 = self.decoder_block(256, 128)  # 4x4 -> 8x8
+        self.decoder1 = self.decoder_block(128, 64)  # 8x8 -> 16x16
+
+        # Skip connection warstwy
+        self.skip_conv3 = nn.Conv2d(512, 256, kernel_size=1)
+        self.skip_conv2 = nn.Conv2d(256, 128, kernel_size=1)
+        self.skip_conv1 = nn.Conv2d(128, 64, kernel_size=1)
+
+        # Warstwa wyjściowa
+        self.final_conv = nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1)  # 16x16 -> 32x32
+
+    def encoder_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+    def bottleneck_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+    def decoder_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+    def forward(self, x):
+        # Enkoder
+        enc1 = self.encoder1(x)  # 32x32 -> 16x16
+        enc2 = self.encoder2(enc1)  # 16x16 -> 8x8
+        enc3 = self.encoder3(enc2)  # 8x8 -> 4x4
+
+        # Bottleneck
+        bottleneck = self.bottleneck(enc3)  # 4x4 -> 2x2
+
+        # Dekoder
+        dec3 = self.decoder3(bottleneck)  # 2x2 -> 4x4
+        dec3 = torch.cat((dec3, enc3), dim=1)  # Skip connection z enc3
+        dec3 = self.skip_conv3(dec3)
+
+        dec2 = self.decoder2(dec3)  # 4x4 -> 8x8
+        dec2 = torch.cat((dec2, enc2), dim=1)  # Skip connection z enc2
+        dec2 = self.skip_conv2(dec2)
+
+        dec1 = self.decoder1(dec2)  # 8x8 -> 16x16
+        dec1 = torch.cat((dec1, enc1), dim=1)  # Skip connection z enc1
+        dec1 = self.skip_conv1(dec1)
+
+        # Warstwa wyjściowa
+        output = self.final_conv(dec1)  # 16x16 -> 32x32
+
+        return output
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        # Warstwy konwolucyjne
+        self.conv1 = self.discriminator_block(1, 64, batch_norm=False)  # 32x32 -> 16x16
+        self.conv2 = self.discriminator_block(64, 128)  # 16x16 -> 8x8
+        self.conv3 = self.discriminator_block(128, 256)  # 8x8 -> 4x4
+        self.conv4 = self.discriminator_block(256, 512)  # 4x4 -> 2x2
+
+        # Warstwa w pełni połączona
+        self.fc = nn.Linear(512 * 2 * 2, 1)  # Zmiana wymiaru na jeden wynik (real/fake)
+        self.sigmoid = nn.Sigmoid()  # Sigmoid do klasyfikacji binarnej (prawdziwy/fałszywy)
+
+    def discriminator_block(self, in_channels, out_channels, batch_norm=True):
+        layers = [
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+        if batch_norm:
+            layers.append(nn.BatchNorm2d(out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)  # 32x32 -> 16x16
+        x = self.conv2(x)  # 16x16 -> 8x8
+        x = self.conv3(x)  # 8x8 -> 4x4
+        x = self.conv4(x)  # 4x4 -> 2x2
+
+        # Flattenowanie i klasyfikacja
+        x = x.view(x.size(0), -1)  # Flatten 512*2*2
+        x = self.fc(x)
+        x = self.sigmoid(x)
+
+        return x
